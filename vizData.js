4c5a6830fb31b18c081c230da713ddfeae019f01
mikey <mikey@hey.com>
2023-07-13 11:50:57 -0500

    push all js into one file

--- vizData.js
+++
@@ -1,9 +0,0 @@
-export function vizData(selection, data) {
-    selection
-    .selectAll('circle')
-    .data(data)
-    .join('circle')
-    .attr('r', (d) => d.r)
-    .attr('cx', (d) => d.x)
-    .attr('cy', (d) => d.y);
-}
--- waves.js
+++ waves.js
@@ -1,19 +1,16 @@
-import { select } from 'd3';
-import { vizData } from './vizData';
-import { makeData } from './makeData';
+import { select, range } from 'd3';
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 const svg = select('body')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height);
 
-let t = 0;
-setInterval(() => {
-    const n = 10 + Math.sin(t) * 5;
-    const data = makeData(n, t);
-    svg.vall(vizData, data);
-    t = t + 0.01;
-}, 1000 / 60);
+const data = range(15);
+
+svg.selectAll('circle')
+  .data(data)
+  .enter()
+  .append('circle');
